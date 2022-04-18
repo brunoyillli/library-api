1d634bf5c39184e48fbf89ed98b06613b5e84f32
Bruno Mendes <wolfteam1910@gmail.com>
2022-04-18 11:02:34 -0300

    validando existencia dde um livro pelo isbn na base de dados

--- src/main/java/com/brunomendes/libraryapi/model/entity/Book.java
+++ src/main/java/com/brunomendes/libraryapi/model/entity/Book.java
@@ -1,5 +1,12 @@
 package com.brunomendes.libraryapi.model.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,10 +16,21 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 @AllArgsConstructor
+@Entity
+@Table
 public class Book {
+	@Id
+	@Column
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id; 
+	
+	@Column
 	private String title;
+	
+	@Column
 	private String author;
+	
+	@Column
 	private String isbn;
 
 }
--- src/test/java/com/brunomendes/libraryapi/model/repository/BookRepositoryTest.java
+++ src/test/java/com/brunomendes/libraryapi/model/repository/BookRepositoryTest.java
@@ -10,6 +10,8 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.brunomendes.libraryapi.model.entity.Book;
+
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
 @DataJpaTest
@@ -25,9 +27,23 @@ public class BookRepositoryTest {
     @DisplayName("Deve retornar verdadeiro quando existir um livro na base com o isbn informado")
 	public void returnTrueWhenIsbnExists() {
 		String isbn = "123";
+		Book book = Book.builder().title("Aventuras").author("Fulano").isbn(isbn).build();
+		entityManager.persist(book);
 		
 		boolean exists = repository.existsByIsbn(isbn);
 	
 		Assertions.assertThat(exists).isTrue();
+	}
+    
+    @Test
+    @DisplayName("Deve retornar false quando não existir um livro na base com o isbn informado")
+	public void returnFalseWhenIsbnExist() {
+		String isbn = "123";
+		Book book = Book.builder().title("Aventuras").author("Fulano").isbn(isbn).build();
+		entityManager.persist(book);
+		
+		boolean exists = repository.existsByIsbn(isbn);
+	
+		Assertions.assertThat(exists).isFalse();
 	}
 }
