285edd8bcf47b8dbe3fa96652647193e688fe25c
Bruno Mendes <wolfteam1910@gmail.com>
2022-04-21 15:41:14 -0300

    teste de integracao do repository do save e delete

--- src/test/java/com/brunomendes/libraryapi/model/repository/BookRepositoryTest.java
+++ src/test/java/com/brunomendes/libraryapi/model/repository/BookRepositoryTest.java
@@ -59,6 +59,31 @@ public class BookRepositoryTest {
     	
     }
     
+    @Test
+    @DisplayName("Deve salvar um livro")
+    public void saveBookTest() {
+    	Book book = createNewBook("123");
+    	
+    	Book savedBook = repository.save(book);
+    	
+    	Assertions.assertThat(savedBook.getId()).isNotNull();
+    }
+    
+    @Test
+    @DisplayName("Deve deletar um livro")
+    public void deleteBookTest() {
+		Book book = createNewBook("123");
+		entityManager.persist(book);
+		
+		Book foundBook = entityManager.find(Book.class, book.getId());
+		
+		repository.delete(foundBook);
+		
+		Book deleteBook = entityManager.find(Book.class, book.getId());
+		Assertions.assertThat(deleteBook).isNull();
+		
+    }
+    
 	private Book createNewBook(String isbn) {
 		return Book.builder().title("Aventuras").author("Fulano").isbn(isbn).build();
 	}
