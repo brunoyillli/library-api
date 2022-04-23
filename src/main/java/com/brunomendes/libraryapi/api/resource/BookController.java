3271dcecb39971c95419a72ae4c35e0558274007
Bruno Mendes <wolfteam1910@gmail.com>
2022-04-23 11:47:51 -0300

    implementacao consulta de livro

--- src/main/java/com/brunomendes/libraryapi/api/resource/BookController.java
+++ src/main/java/com/brunomendes/libraryapi/api/resource/BookController.java
@@ -1,8 +1,14 @@
 package com.brunomendes.libraryapi.api.resource;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import javax.validation.Valid;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -74,6 +80,16 @@ public class BookController {
         }).orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND) );
 	}
 	
+	@GetMapping
+	public Page<BookDTO> find( BookDTO dto, Pageable pageRequest ){
+		Book filter = modelMapper.map(dto, Book.class);
+		Page<Book> result = service.find(filter, pageRequest);
+		List<BookDTO> list = result.getContent()
+				.stream()//
+				.map( entity -> modelMapper.map(entity, BookDTO.class ) )
+				.collect( Collectors.toList() );
+		return new PageImpl<BookDTO>( list, pageRequest, result.getTotalElements() );
+	}
 	
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
