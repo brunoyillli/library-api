30e15c0d12d4d054fdca61a5c89981ddd6cadbd6
Bruno Mendes <wolfteam1910@gmail.com>
2022-04-23 12:38:52 -0300

    implementacao do filtro de livros com example

--- src/main/java/com/brunomendes/libraryapi/service/impl/BookServiceImpl.java
+++ src/main/java/com/brunomendes/libraryapi/service/impl/BookServiceImpl.java
@@ -2,6 +2,8 @@ package com.brunomendes.libraryapi.service.impl;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -51,8 +53,15 @@ public class BookServiceImpl implements BookService {
 
 	@Override
 	public Page<Book> find(Book filter, Pageable pageRequest) {
-		// TODO Auto-generated method stub
-		return null;
+		Example<Book> example = Example.of(filter,
+				ExampleMatcher
+					.matching()
+					.withIgnoreCase()
+					.withIgnoreNullValues()
+					.withStringMatcher( ExampleMatcher.StringMatcher.CONTAINING )
+		);
+		
+		return repository.findAll(example, pageRequest);
 	}
 
 }
--- src/test/java/com/brunomendes/libraryapi/service/BookServiceTest.java
+++ src/test/java/com/brunomendes/libraryapi/service/BookServiceTest.java
@@ -3,6 +3,8 @@ package com.brunomendes.libraryapi.service;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import org.assertj.core.api.Assertions;
@@ -12,6 +14,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -150,6 +156,26 @@ public class BookServiceTest {
     	assertThat(book.getTitle()).isEqualTo(updateBook.getTitle());
     	assertThat(book.getIsbn()).isEqualTo(updateBook.getIsbn());
     	assertThat(book.getAuthor()).isEqualTo(updateBook.getAuthor());    		
+    }
+    @Test
+    @DisplayName("Deve filtrar livros pelas propriedades")
+    public void findBookTest() {
+    	
+    	Book book = createValidBook();
+    	book.setId(1l);
+        PageRequest pageRequest = PageRequest.of(0, 10);
+
+        List<Book> lista = Arrays.asList(book);
+        Page<Book> page = new PageImpl<Book>(lista, pageRequest, 1);
+    	
+    	Mockito.when( repository.findAll(Mockito.any(Example.class), Mockito.any(PageRequest.class)))
+        .thenReturn(page);
+    	Page<Book> result = service.find( book, pageRequest );
+    	
+    	assertThat(result.getTotalElements()).isEqualTo(1);
+    	assertThat(result.getContent()).isEqualTo(lista);
+    	assertThat(result.getPageable().getPageNumber()).isEqualTo(0);
+    	assertThat(result.getPageable().getPageSize()).isEqualTo(10);
     }
     
     private Book createValidBook() {
