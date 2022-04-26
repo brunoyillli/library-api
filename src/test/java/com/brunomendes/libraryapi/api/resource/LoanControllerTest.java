272a6baf21f0c8857693a3afbfbeea6f712d66f3
Bruno Mendes <wolfteam1910@gmail.com>
2022-04-26 09:23:46 -0300

    ajustes e implementacao do teste da validacao de livro ja emprestado

--- src/test/java/com/brunomendes/libraryapi/api/resource/LoanControllerTest.java
+++ src/test/java/com/brunomendes/libraryapi/api/resource/LoanControllerTest.java
@@ -25,6 +25,7 @@ import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilde
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import com.brunomendes.libraryapi.api.dto.LoanDTO;
+import com.brunomendes.libraryapi.exception.BusinessException;
 import com.brunomendes.libraryapi.model.entity.Book;
 import com.brunomendes.libraryapi.model.entity.Loan;
 import com.brunomendes.libraryapi.service.BookService;
@@ -52,7 +53,7 @@ public class LoanControllerTest {
 	@DisplayName("Deve realizar um emprestimo ")
 	public void createLoanTest() throws Exception{
 		Book book = Book.builder().id(1l).isbn("123").build();
-		Loan loan = Loan.builder().id(1l).custumer("Fulano").book(book).loanDate(LocalDate.now()).build();
+		Loan loan = Loan.builder().id(1l).customer("Fulano").book(book).loanDate(LocalDate.now()).build();
 		LoanDTO dto = LoanDTO.builder().isbn("123").customer("Fulano").build();
 		String json = new ObjectMapper().writeValueAsString(dto);
 		
@@ -72,24 +73,49 @@ public class LoanControllerTest {
 		
 	}
 	
-	@Test
-	@DisplayName("Deve retornar erro ao tentar fazer emprestimo de um livro inexistente")
-	public void invalidIsbnCreateLoanTest() throws Exception{
-		
-		LoanDTO dto = LoanDTO.builder().isbn("123").customer("Fulano").build();
-		String json = new ObjectMapper().writeValueAsString(dto);
-		
-		BDDMockito.given( bookService.getBookByIsbn("123") ).willReturn( Optional.empty() );
-		
-		MockHttpServletRequestBuilder request = MockMvcRequestBuilders.post( LOAN_API )
-				.accept(MediaType.APPLICATION_JSON)
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(json);
-		
-		mvc.perform(request)
-				.andExpect( status().isBadRequest() )
-				.andExpect( jsonPath("errors", Matchers.hasSize(1)) )
-				.andExpect( jsonPath("errors[0]").value("Book not found for isbn"))
-				;
-	}
+    @Test
+    @DisplayName("Deve retornar erro ao tentar fazer emprestimo de um livro inexistente.")
+    public void invalidIsbnCreateLoanTest() throws  Exception{
+
+        LoanDTO dto = LoanDTO.builder().isbn("123").customer("Fulano").build();
+        String json = new ObjectMapper().writeValueAsString(dto);
+
+        BDDMockito.given( bookService.getBookByIsbn("123") ).willReturn( Optional.empty() );
+
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders.post(LOAN_API)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mvc.perform( request )
+                .andExpect( status().isBadRequest() )
+                .andExpect( jsonPath("errors", Matchers.hasSize(1)) )
+                .andExpect( jsonPath("errors[0]").value("Book not found for passed isbn"))
+        ;
+    }
+	
+    @Test
+    @DisplayName("Deve retornar erro ao tentar fazer emprestimo de um livro emprestado.")
+    public void loanedBookErrorOnCreateLoanTest() throws  Exception{
+
+        LoanDTO dto = LoanDTO.builder().isbn("123").customer("Fulano").build();
+        String json = new ObjectMapper().writeValueAsString(dto);
+
+        Book book = Book.builder().id(1l).isbn("123").build();
+        BDDMockito.given( bookService.getBookByIsbn("123") ).willReturn(Optional.of(book) );
+
+        BDDMockito.given( loanService.save(Mockito.any(Loan.class)) )
+                .willThrow(new BusinessException("Book already loaned"));
+
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders.post(LOAN_API)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mvc.perform( request )
+                .andExpect( status().isBadRequest() )
+                .andExpect( jsonPath("errors", Matchers.hasSize(1)) )
+                .andExpect( jsonPath("errors[0]").value("Book already loaned"))
+        ;
+    }
 }
