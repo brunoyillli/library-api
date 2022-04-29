009aa9945b1fddd3c8a5491a0a001c3e114cd624
Bruno Mendes <wolfteam1910@gmail.com>
2022-04-29 09:10:03 -0300

    lancar erro ao tentar fazer emprestimo de livro ja emprestado

--- src/main/java/com/brunomendes/libraryapi/model/repository/LoanRepository.java
+++ src/main/java/com/brunomendes/libraryapi/model/repository/LoanRepository.java
@@ -2,8 +2,11 @@ package com.brunomendes.libraryapi.model.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.brunomendes.libraryapi.model.entity.Book;
 import com.brunomendes.libraryapi.model.entity.Loan;
 
 public interface LoanRepository extends JpaRepository<Loan, Long>{
+
+	boolean existsByBookAndNotReturned(Book book);
 
 }
--- src/main/java/com/brunomendes/libraryapi/service/impl/LoanServiceImpl.java
+++ src/main/java/com/brunomendes/libraryapi/service/impl/LoanServiceImpl.java
@@ -1,5 +1,6 @@
 package com.brunomendes.libraryapi.service.impl;
 
+import com.brunomendes.libraryapi.exception.BusinessException;
 import com.brunomendes.libraryapi.model.entity.Loan;
 import com.brunomendes.libraryapi.model.repository.LoanRepository;
 import com.brunomendes.libraryapi.service.LoanService;
@@ -14,6 +15,9 @@ public class LoanServiceImpl implements LoanService {
 
 	@Override
 	public Loan save(Loan loan) {
+		if( repository.existsByBookAndNotReturned(loan.getBook()) ) {
+			throw new BusinessException("Book already loaned");
+		}
 		return repository.save(loan);
 	}
 
