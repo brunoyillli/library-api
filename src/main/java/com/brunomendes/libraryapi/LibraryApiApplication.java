298d05b8a2b6b4105279a53cf1daa933015533c5
Bruno Mendes <wolfteam1910@gmail.com>
2022-05-06 19:01:17 -0300

    testando agendamento de tarefas com schedules do spring, funcionou

--- src/main/java/com/brunomendes/libraryapi/LibraryApiApplication.java
+++ src/main/java/com/brunomendes/libraryapi/LibraryApiApplication.java
@@ -4,8 +4,11 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
+@EnableScheduling
 public class LibraryApiApplication {
 
 	@Bean
@@ -13,7 +16,10 @@ public class LibraryApiApplication {
 		return new ModelMapper();
 	}
 	
-	
+	@Scheduled(cron = "0 0 19 1/1 * ?")
+	public void testeAgendamentoTarefas() {
+		System.out.println("testando scheduled com sucesso");
+	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApiApplication.class, args);
