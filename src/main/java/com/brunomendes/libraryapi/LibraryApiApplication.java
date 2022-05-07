8c3d88c5b240ae70ad649ca12b07b279557c9013
Bruno Mendes <wolfteam1910@gmail.com>
2022-05-07 20:29:44 -0300

    implementando servico de email

--- src/main/java/com/brunomendes/libraryapi/LibraryApiApplication.java
+++ src/main/java/com/brunomendes/libraryapi/LibraryApiApplication.java
@@ -5,21 +5,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
 @EnableScheduling
 public class LibraryApiApplication {
-
+	
 	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
 	}
 	
-	@Scheduled(cron = "0 0 19 1/1 * ?")
-	public void testeAgendamentoTarefas() {
-		System.out.println("testando scheduled com sucesso");
-	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApiApplication.class, args);
